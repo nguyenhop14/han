8c3f7452df7dab5a7c09bcc86c5c1302933854eb
nguyenhop14 <nguyenhop0104@gmail.com>
2019-07-24 14:21:18 +0700

    1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,69 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by params[:id]
+    redirect_to root_path and return unless true
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:into] = "Please check your email to activate your account"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find_by(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in"
+        redirect_to login_path
+      end
+    end
+
+    def correct_user
+      @user = User.find_by(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'hopnt98@gmail.com'
+  layout 'mailer'
+end
